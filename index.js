6357a67654acae8a6b338ef40f7fd0933b374f84
Miguel Huayhua <miguelhuayhuac@gmail.com>
2026-07-08 21:42:20 -0400

    version 1.0.1

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ server.use(express.json())
 
 server.use(morgan("dev"))
 server.use(cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true
 }))
 server.use(cookieParser())
