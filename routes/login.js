61562eb58b3ba0d22f48952ec432093ddbf705cb
Miguel Huayhua <miguelhuayhuac@gmail.com>
2026-07-08 22:31:54 -0400

    version 1.0.2

--- routes/login.js
+++ routes/login.js
@@ -40,7 +40,7 @@ LOGIN_ROUTER.post('/signin', async (req, res) => {
                 token: refreshToken, expira_en: new Date(), usuario_id: usuario.id
             }
         })
-        res.cookie("refreshToken", refreshToken);
+        res.cookie("refreshToken", refreshToken, { httpOnly: true, sameSite: "none", secure: true, maxAge: 60 * 60 * 24 * 7 });
         return res.json({ accessToken, estado: 200, mensaje: "Usuario logueado exitosamente" }).status(200);
     }
     else {
